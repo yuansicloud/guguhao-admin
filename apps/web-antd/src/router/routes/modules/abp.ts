f2c5447d4a13e1d93b2c623b3e31845238c6f32b
slarkerino <wanjackcool@qq.com>
2025-01-13 18:59:54 +0800

    feat: 新增管理模块

---
+++ apps/web-antd/src/router/routes/modules/abp.ts
@@ -0,0 +1,60 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { BasicLayout } from '#/layouts';
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    component: BasicLayout,
+    meta: {
+      icon: 'https://abp.io/assets/favicon.ico/favicon-16x16.png',
+      keepAlive: true,
+      order: 1000,
+      title: $t('abp.title'),
+    },
+    name: 'AbpFramework',
+    path: '/abp',
+    children: [
+      {
+        meta: {
+          title: $t('abp.manage.title'),
+          icon: 'arcticons:activity-manager',
+        },
+        name: 'Manage',
+        path: '/manage',
+        children: [
+          {
+            meta: {
+              title: $t('abp.manage.identity.title'),
+              icon: 'teenyicons:id-outline',
+            },
+            name: 'Identity',
+            path: '/manage/identity',
+            children: [
+              {
+                component: () => import('#/views/identity/users/index.vue'),
+                name: 'Users',
+                path: '/manage/identity/users',
+                meta: {
+                  title: $t('abp.manage.identity.user'),
+                  icon: 'mdi:user-outline',
+                },
+              },
+              {
+                component: () => import('#/views/identity/roles/index.vue'),
+                name: 'Roles',
+                path: '/manage/identity/roles',
+                meta: {
+                  title: $t('abp.manage.identity.role'),
+                  icon: 'carbon:user-role',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default routes;
